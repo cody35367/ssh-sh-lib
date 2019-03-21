91085bd9c4f8a651ccb718e506007f4cdf8301f7
Cody Hodges <chodges@tietronix.com>
2019-03-21 06:37:31 -0500

    Added quote to better handle spaces

--- filezilla.sh
+++ filezilla.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ -z ${SCS_SSH_PRIVATEKEY} ]]; then
-    nohup ${SCS_FILEZILLA_PATH} -l ask ${SCS_FILEZILLA_PROTOCOL}://${SCS_SSH_USERNAME}@${SCS_SSH_SERVER} &> /dev/null &
+    nohup "${SCS_FILEZILLA_PATH}" -l ask ${SCS_FILEZILLA_PROTOCOL}://${SCS_SSH_USERNAME}@${SCS_SSH_SERVER} &> /dev/null &
 else
-    nohup ${SCS_FILEZILLA_PATH} -c 0/${SCS_FILEZILLA_NAME} &> /dev/null &
+    nohup "${SCS_FILEZILLA_PATH}" -c "0/${SCS_FILEZILLA_NAME}" &> /dev/null &
 fi
