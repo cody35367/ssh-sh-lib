342648b9011a9d51324a3e043aaa86d2c949e369
Cody Hodges <chodges@tietronix.com>
2019-03-27 11:06:25 -0500

    Add new ssh connect profile and also changed how filezilla determines to use name or not.

--- filezilla.sh
+++ filezilla.sh
@@ -2,7 +2,7 @@
 if [[ ${SCS_VERBOSE_MODE} = true ]]; then
     set -x
 fi
-if [[ -z ${SCS_SSH_PRIVATEKEY} ]]; then
+if [[ -z ${SCS_FILEZILLA_NAME} ]]; then
     nohup "${SCS_FILEZILLA_PATH}" -l ask ${SCS_FILEZILLA_PROTOCOL}://${SCS_SSH_USERNAME}@${SCS_SSH_SERVER} &> /dev/null &
 else
     nohup "${SCS_FILEZILLA_PATH}" -c "0/${SCS_FILEZILLA_NAME}" &> /dev/null &
